2b19e030bbf5caa0c00b3778513ccbcd99b16512
Maimounah97 <maealhejaili@gmail.com>
2023-09-03 14:01:51 +0300

    Updating 9-3

--- Dashboard/Controllers/Shopping/ShoppingController.cs
+++ Dashboard/Controllers/Shopping/ShoppingController.cs
@@ -18,42 +18,59 @@ namespace Dashboard.Controllers.Shopping
         }
         public IActionResult Index()
 		{
-
-			return View();
+            var Products = _context.Product.ToList();
+            var ProductDetails = _context.ProductDetails.ToList();
+            ViewBag.Name = Request.Cookies["Name"];
+            ViewBag.ProductDetails = ProductDetails;
+            return View(Products);
 		}
-		//public async Task<string> SendMail()
-		//{
-		//	//xxoiqetkzpzyuxzc
-		//	//nzufhzxvbmvqyves
+        public IActionResult ProductDetails(int id, int productId)
+        {
+            var product = _context.Product.SingleOrDefault(p => p.Id == productId);
+            var ProductDetails = _context.ProductDetails.SingleOrDefault(pd => pd.Id == id);
+            var ProductDetailsList = _context.ProductDetails.ToList();
+            ViewBag.ProductDetails = ProductDetails;
+            ViewBag.ProductDetailsList = ProductDetailsList;
+            ViewBag.Product = product;
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        //public async Task<string> SendMail()
+        //{
+        //	//xxoiqetkzpzyuxzc
+        //	//nzufhzxvbmvqyves
 
-		//	var message = new MimeMessage();
-		//	message.From.Add(new MailboxAddress("TestMessage", "tuwiqst@gmail.com"));
-		//	message.To.Add(MailboxAddress.Parse("ssyd12@hotmail.com"));
-		//	message.Subject = "test email from asp.net ";
-		//	message.Body = new TextPart("plain")
-		//	{
-		//		Text = "Welcom"
-		//	};
-		//	using (var client = new SmtpClient())
-		//	{
-		//		try
-		//		{
-		//			client.Connect("smtp.gmail.com", 587);
-		//			client.Authenticate("tuwiqst@gmail.com", "nzufhzxvbmvqyves");
-		//			await client.SendAsync(message);
-		//			client.Disconnect(true);
+        //	var message = new MimeMessage();
+        //	message.From.Add(new MailboxAddress("TestMessage", "h.maimounah@gmail.com"));
+        //	message.To.Add(MailboxAddress.Parse("maealhejaili@gmail.com"));
+        //	message.Subject = "test email from asp.net ";
+        //	message.Body = new TextPart("plain")
+        //	{
+        //		Text = "Welcom"
+        //	};
+        //	using (var client = new SmtpClient())
+        //	{
+        //		try
+        //		{
+        //			client.Connect("smtp.gmail.com", 587);
+        //			client.Authenticate("tuwiqst@gmail.com", "nzufhzxvbmvqyves");
+        //			await client.SendAsync(message);
+        //			client.Disconnect(true);
 
-		//		}
-		//		catch (Exception e)
-		//		{
-		//			Console.WriteLine(e.Message.ToString());
-		//		}
+        //		}
+        //		catch (Exception e)
+        //		{
+        //			Console.WriteLine(e.Message.ToString());
+        //		}
 
 
 
-		//	};
+        //	};
 
-		//	return "ok";
-		//}
-	}
+        //	return "ok";
+        //}
+    }
 }
--- Dashboard/Program.cs
+++ Dashboard/Program.cs
@@ -48,7 +48,7 @@ internal class Program
 
 		app.MapControllerRoute(
 			name: "default",
-			pattern: "{controller=Home}/{action=Index}/{id?}");
+			pattern: "{controller=Shopping}/{action=Index}/{id?}");
 		app.MapRazorPages();
 		app.UseDeveloperExceptionPage();
 		app.Run();
